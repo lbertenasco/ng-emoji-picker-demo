c44003130a85a00066cfd0091489a10a097f5223
Luciano Bertenasco <lbertenasco@gmail.com>
2018-01-22 12:01:48 -0300

    New ng emoji picker demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'app';
+  text: string = '';
+  openPopup: Function;
+
+  setPopupAction(fn: any) {
+    console.log('setPopupAction');
+    this.openPopup = fn;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { EmojiPickerModule } from 'ng-emoji-picker';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EmojiPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
